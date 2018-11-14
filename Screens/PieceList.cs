dfaff4bb5483fe00bc61242b8e8d902ebc07af09
Lehkikm <Mikhael1729@gmail.com>
2018-11-14 13:27:24 -0800

    refactor: A list much more prettier.

--- Screens/PieceList.cs
+++ Screens/PieceList.cs
@@ -3,19 +3,28 @@ using IleanaMusic.Models;
 using System;
 using IleanaMusic.Data;
 using System.Linq;
-
+using System.Collections.Generic;
 
 namespace IleanaMusic.Screens 
 {
     public class PieceList 
     {
-
+        List<Piece> pieces = AppData.Instance.PieceList; 
+        
         public PieceList() 
         {  
-            WriteLine("Listado de canciones: \n");
-             foreach (var piece in AppData.Instance.PieceList)
+            WriteLine(
+                "Lista de canciones\n"
+               +"------------------\n");
+
+            if(pieces.Count > 0) 
             {
-                WriteLine(piece.Name);
+                foreach (var piece in AppData.Instance.PieceList)
+                    WriteLine($"{piece.Id}. {piece.Name}");
+            } 
+            else
+            {
+                WriteLine("\n >> Lista vacía <<");
             }
         }
     }
