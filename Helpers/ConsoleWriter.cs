7c36ab2ba6406bd6ba7fc1426d528aa9df7b7593
Lehkikm <Mikhael1729@gmail.com>
2018-12-09 10:43:52 -0800

    Fixed a problem with ConsoleWriter

--- Helpers/ConsoleWriter.cs
+++ Helpers/ConsoleWriter.cs
@@ -5,15 +5,17 @@ namespace IleanaMusic.Helpers
     public class ConsoleWriter 
     {
         public string Spaces { get; private set; }
+        public int Indentation { get; set; }
 
-        public ConsoleWriter(int spaceQuantity) 
+        public ConsoleWriter(int spaceQuantity, int indentation = 3) 
         {
             Spaces = GenerateSpaces(spaceQuantity);
+            Indentation = indentation;
         }
 
         public void Write(string text, int indent = 0, bool spaceBefore = false, bool cancelSpace = false) 
         {
-            var extra = GenerateSpaces(indent * Spaces.Length);
+            var extra = GenerateSpaces(indent * (Spaces.Length > 0 ? Spaces.Length : Indentation));
 
             System.Console.Write(
                 (spaceBefore ? "\n" : "") 
