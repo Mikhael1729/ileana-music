07316db0be4b7d2bad578da4334a9c4633becb8c
Lehkikm <Mikhael1729@gmail.com>
2018-12-09 13:23:33 -0800

    feat: Added PieceService into AppData.

--- Data/AppData.cs
+++ Data/AppData.cs
@@ -1,39 +1,47 @@
 using IleanaMusic.Models;
 using System.Collections.Generic;
 using System;
+using IleanaMusic.Data.Services;
 
-namespace IleanaMusic.Data 
+namespace IleanaMusic.Data
 {
-    public class AppData 
+    public class AppData
     {
         private static readonly Lazy<AppData> instance = new Lazy<AppData>(() => new AppData());
-        public List<Piece> PieceList = new List<Piece>() 
-        {
-            new Piece() {
-                Id = 1,
-                Name = "Pieza 1",
-                Album ="Album 1",
-                Artist = "Artista 1",
-                Duration = 23,
-                Quality = Quality.High,
-                Format = MusicFormat.Mp3,
-                Gender = Gender.Classical
-            },
-            new Piece() {
-                Id = 2,
-                Name = "Pieza 2",
-                Album ="Album 2",
-                Artist = "Artista 2",
-                Duration = 23,
-                Quality = Quality.High,
-                Format = MusicFormat.Mp3,
-                Gender = Gender.Classical
-            },
-        };
-        public List<Playlist> Playlists = new List<Playlist>() {};
-        
+        public List<Piece> PieceList;
+        public List<Playlist> Playlists;
+
+        public PieceService PieceService;
+
         private AppData()
         {
+            PieceList = new List<Piece>()
+            {
+                new Piece() {
+                    Id = 1,
+                    Name = "Pieza 1",
+                    Album ="Album 1",
+                    Artist = "Artista 1",
+                    Duration = 23,
+                    Quality = Quality.High,
+                    Format = MusicFormat.Mp3,
+                    Gender = Gender.Classical
+                },
+                new Piece() {
+                    Id = 2,
+                    Name = "Pieza 2",
+                    Album ="Album 2",
+                    Artist = "Artista 2",
+                    Duration = 23,
+                    Quality = Quality.High,
+                    Format = MusicFormat.Mp3,
+                    Gender = Gender.Classical
+                },
+            };
+
+            Playlists = new List<Playlist>();
+            
+            PieceService = new PieceService(data: PieceList);
         }
 
         public static AppData Instance
