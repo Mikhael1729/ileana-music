f569829ae2b70fd00bb9d569454f5c0f75f3c602
Lehkikm <Mikhael1729@gmail.com>
2018-12-09 08:36:11 -0800

    refactor: Now, printing the searched piece with Piece.Print() method.

--- Screens/EditPieceScreen.cs
+++ Screens/EditPieceScreen.cs
@@ -5,40 +5,66 @@ using IleanaMusic.Data;
 using IleanaMusic.Models;
 using static System.Console;
 
-namespace IleanaMusic.Screens 
+namespace IleanaMusic.Screens
 {
-    public class EditPieceScreen 
+    public class EditPieceScreen
     {
         List<Piece> pieceList = AppData.Instance.PieceList;
+        Piece piece;
 
         public EditPieceScreen()
         {
             // Title
             WriteLine(
                 "Edita una canción\n"
-               +"-----------------\n");
+               + "-----------------\n");
 
             // Requesting Id.
             int pieceId = 0;
-            Piece piece = null;
 
-            if(pieceList.Count > 0) 
+            if (pieceList.Count > 0)
             {
-                while(pieceId == 0) 
+                while (pieceId == 0)
                 {
-                    Write("- ID de la canción: ");  
+                    Write("Escribe el ID o el Nombre de tu canción: ");
 
-                    if(Int32.TryParse(ReadLine(), out pieceId))
+                    string option;
+                    string name = "";
+                    int id = 0;
+
+                    option = ReadLine();
+
+                    WriteLine("");
+
+                    // Si fue ID.
+                    if (Int32.TryParse(option, out id))
+                    {
+                        piece = (pieceList.Where(p => p.Id == id)).FirstOrDefault();
+                    }
+                    else  // Si fue nombre
+                    {
+                        name = option;
+                        piece = (pieceList.Where(p => p.Name.ToLower() == name.ToLower())).FirstOrDefault();
+                    }
+
+                    if (piece != null)
+                    {
+                        WriteLine(">> ¡Pieza encontrada!\n");
+
+                        piece.Print();
+                    }
+
+                    if (Int32.TryParse(ReadLine(), out pieceId))
                     {
                         piece = pieceList.Where(i => i.Id == pieceId).FirstOrDefault();
-                    }      
+                    }
                 }
 
-                if(piece == null) 
+                if (piece == null)
                 {
                     WriteLine("\n>> Canción no encontrada <<\n");
                 }
-                else 
+                else
                 {
                     WriteLine($"\n >> Editando la pieza \"{piece.Name}\" <<\n");
 
@@ -53,25 +79,25 @@ namespace IleanaMusic.Screens
 
 
                     int genderOption = 0;
-                    while(genderOption == 0 && genderOption < 3)
+                    while (genderOption == 0 && genderOption < 3)
                     {
                         WriteLine("- Género: ");
-                        Write("    1. Música clásica: \n" + 
-                                "    2. Rock: \n" + 
+                        Write("    1. Música clásica: \n" +
+                                "    2. Rock: \n" +
                                 "    3. Raggeton:\n\n" +
                                 "    Escoje [1-3]: ");
 
-                        if(Int32.TryParse(ReadLine(), out genderOption))
+                        if (Int32.TryParse(ReadLine(), out genderOption))
                         {
                             switch (genderOption)
                             {
                                 case 1:
                                     piece.Gender = Gender.Classical;
                                     break;
-                                case 2: 
+                                case 2:
                                     piece.Gender = Gender.Raggeton;
                                     break;
-                                case 3: 
+                                case 3:
                                     piece.Gender = Gender.Rock;
                                     break;
                             }
@@ -79,35 +105,35 @@ namespace IleanaMusic.Screens
                     }
 
                     double duration = 0;
-                    while(duration == 0) 
+                    while (duration == 0)
                     {
                         Write("- Duración: ");
 
-                        if(Double.TryParse(ReadLine(), out duration)) 
+                        if (Double.TryParse(ReadLine(), out duration))
                             piece.Duration = duration;
                     }
 
 
                     var quality = 0;
-                    while(quality == 0) 
+                    while (quality == 0)
                     {
-                        Write("- Calidad: \n" 
+                        Write("- Calidad: \n"
                                 + "    1. Baja. \n"
                                 + "    2. Media\n"
                                 + "    3. Alta \n\n"
                                 + "    Escoje [1-3]: ");
-                        
-                        if(Int32.TryParse(ReadLine(), out quality))
+
+                        if (Int32.TryParse(ReadLine(), out quality))
                         {
                             switch (quality)
                             {
-                                case 1: 
+                                case 1:
                                     piece.Quality = Quality.Low;
                                     break;
-                                case 2: 
+                                case 2:
                                     piece.Quality = Quality.Medium;
                                     break;
-                                case 3: 
+                                case 3:
                                     piece.Quality = Quality.High;
                                     break;
                             }
@@ -115,13 +141,13 @@ namespace IleanaMusic.Screens
                     }
 
                     WriteLine("\n-->> Pieza editada <<--\n");
-                
+
                 }
             }
-            else 
+            else
             {
                 WriteLine(">> Tu lista de canciones está vacía. Agrega una para habilitar esta sección");
             }
-        } 
+        }
     }
 }
