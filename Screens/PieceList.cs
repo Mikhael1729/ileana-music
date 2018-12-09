32cfbc85e77992614cdf2d26decd58d79459c71e
Lehkikm <Mikhael1729@gmail.com>
2018-12-09 13:55:25 -0800

    refactor: Using Print method in PieceList.

--- Screens/PieceList.cs
+++ Screens/PieceList.cs
@@ -21,59 +21,9 @@ namespace IleanaMusic.Screens
             {
                 foreach (var piece in AppData.Instance.PieceList)
                 {
-                    WriteLine($"- ID: {piece.Id}");
-                    WriteLine($"  Nombre: {piece.Name}");
-                    WriteLine($"  Artista: {piece.Artist}");
-                    WriteLine($"  Álbum: {piece.Album}");
-                    Write($"  Género: Música ");
-                    switch (piece.Gender)
-                    {
-                        case Gender.Classical:
-                            Write("Clásica");
-                            break;
-                        case Gender.Raggeton:
-                            Write("Raggeton");
-                            break;
-                        case Gender.Rock:
-                            Write("Rock");
-                            break;
-                    }
-
-                    WriteLine("");
-
-                    WriteLine($"  Duración: {piece.Duration} minutos");
-
-                    Write($"  Calidad: ");
-                    switch (piece.Quality)
-                    {
-                        case Quality.High:
-                            Write("Alta");
-                            break;
-                        case Quality.Low:
-                            Write("Baja");
-                            break;
-                        case Quality.Medium:
-                            Write("Media");
-                            break;
-                    }
-
-                    WriteLine("");
-
-                    Write("  Formato: ");
-                    switch (piece.Format)
-                    {
-                        case MusicFormat.Mp3:
-                            Write("Mp3");
-                            break;
-                        case MusicFormat.Mp4:
-                            Write("Mp4");
-                            break;
-                    }
-
-                    WriteLine("");
+                    piece.Print();
                     WriteLine("");
                 }
-
             }
             else
             {
