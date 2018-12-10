74505623e8e9069799a93d2c5c7741985b0c2bfc
Lehkikm <Mikhael1729@gmail.com>
2018-12-09 16:15:03 -0800

    refactor: Implemented IData<Piece> in PieceService.

--- Data/Services/PieceService.cs
+++ Data/Services/PieceService.cs
@@ -1,12 +1,15 @@
 using System.Collections.Generic;
+using System.Xml.Linq;
+using System.Linq;
 using IleanaMusic.Data.Repositories;
 using IleanaMusic.Models;
 
-namespace IleanaMusic.Data.Services 
+namespace IleanaMusic.Data.Services
 {
     public class PieceService : BaseRepository<Piece>, IPieceService
     {
-        public PieceService(List<Piece> data) : base(data)
-        { }
+        public PieceService(IData<Piece> data) : base(data)
+        {
+        }
     }
 }
