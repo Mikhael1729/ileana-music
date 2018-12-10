4cfca73fc898762ace152688f631aac4f8a38655
Lehkikm <Mikhael1729@gmail.com>
2018-12-09 16:14:05 -0800

    refactor: Implemented in Repository IData.
    Now, the repository has the ability to save changes in data list.

--- Data/Repositories/Repository.cs
+++ Data/Repositories/Repository.cs
@@ -6,9 +6,9 @@ namespace IleanaMusic.Data.Repositories
 {
     public class BaseRepository<T> : IRepository<T> where T : BaseEntity
     {
-        List<T> _data;
+        IData<T> _data;
         
-        public BaseRepository(List<T> data)
+        public BaseRepository(IData<T> data)
         {
             _data = data;
         }
@@ -17,33 +17,35 @@ namespace IleanaMusic.Data.Repositories
         {
             var id = 1;
 
-            if (_data.Count > 0)
-                id = _data[_data.Count - 1].Id + 1;
+            if (_data.List.Count > 0)
+                id = _data.List[_data.List.Count - 1].Id + 1;
 
             entity.Id = id;
-            _data.Add(entity);
+            _data.List.Add(entity);
+
+            _data.SaveAll();
 
             return entity;
         }
 
         public void Delete(T entity)
         {
-            _data.Remove(entity);
+            _data.List.Remove(entity);
         }
 
         public T Get(int id)
         {
-            return _data.Where(d => d.Id == id).FirstOrDefault();
+            return _data.List.Where(d => d.Id == id).FirstOrDefault();
         }
 
         public List<T> GetAll()
         {
-            return _data;
+            return _data.List;
         }
 
         public T Update(T entity)
         {
-            var searchedEntity = _data.Where(d => d.Id == entity.Id).FirstOrDefault();
+            var searchedEntity = _data.List.Where(d => d.Id == entity.Id).FirstOrDefault();
             searchedEntity = entity;
 
             return searchedEntity;
