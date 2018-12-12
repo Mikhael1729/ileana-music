0dd0cc944c7b3df94328ddc14750da701994aa28
Lehkikm <Mikhael1729@gmail.com>
2018-12-12 00:57:03 -0800

    feat: searchPieceInPlaylistScreen is working with serialized data.

--- Screens/SearchPieceInPlaylistScreen.cs
+++ Screens/SearchPieceInPlaylistScreen.cs
@@ -4,14 +4,15 @@ using IleanaMusic.Models;
 using System.Linq;
 using IleanaMusic.Data;
 using System.Collections.Generic;
-
+using IleanaMusic.Data.Services;
 
 namespace IleanaMusic.Screens
 {
     public class SearchPieceInPlaylistScreen
     {
+        PlaylistService playlistService = AppData.Instance.PlaylistService;
+        PieceService pieceService = AppData.Instance.PieceService;
         List<Playlist> playlists = AppData.Instance.Playlists;
-        List<Piece> pieceList = AppData.Instance.PieceService.GetAll();
         Playlist searchedPlaylist;
         Piece searchedPiece;
 
@@ -22,7 +23,7 @@ namespace IleanaMusic.Screens
                     + "------------------------\n");
 
 
-            if (playlists.Count > 0)
+            if (playlistService.Count() > 0)
             {
                 Write("- Escribe el ID o el Nombre de la playlist: ");
                 string option;
@@ -36,12 +37,12 @@ namespace IleanaMusic.Screens
                 // Si fue ID.
                 if (Int32.TryParse(option, out id))
                 {
-                    searchedPlaylist = (playlists.Where(p => p.Id == id)).FirstOrDefault();
+                    searchedPlaylist = playlistService.Get(id);
                 }
                 else  // Si fue nombre
                 {
                     name = option;
-                    searchedPlaylist = (playlists.Where(p => p.Name.ToLower() == name.ToLower())).FirstOrDefault();
+                    searchedPlaylist = playlistService.Find(p => p.Name.ToLower() == name.ToLower());
                 }
 
                 if (searchedPlaylist != null)
@@ -51,9 +52,7 @@ namespace IleanaMusic.Screens
                     Write("- Escribe el ID o el Nombre de la canción: ");
 
                     string pieceOption;
-                    string pieceName = "";
-                    int pieceId = 0;
-
+                    
                     pieceOption = ReadLine();
 
                     WriteLine("");
@@ -61,67 +60,19 @@ namespace IleanaMusic.Screens
                     // Si fue ID.
                     if (Int32.TryParse(pieceOption, out id))
                     {
-                        searchedPiece = (pieceList.Where(p => p.Id == id)).FirstOrDefault();
+                        searchedPiece = pieceService.Get(id);
                     }
                     else  // Si fue nombre
                     {
                         name = pieceOption;
-                        searchedPiece = (pieceList.Where(p => p.Name.ToLower() == name.ToLower())).FirstOrDefault();
+                        searchedPiece = pieceService.Find(p => p.Name.ToLower() == name.ToLower());
                     }
 
                     if (searchedPiece != null)
                     {
                         WriteLine(">> ¡Tu pieza ha sido encontada!\n");
 
-                        WriteLine($"- ID: {searchedPiece.Id}");
-                        WriteLine($"  Nombre: {searchedPiece.Name}");
-                        WriteLine($"  Artista: {searchedPiece.Artist}");
-                        WriteLine($"  Álbum: {searchedPiece.Album}");
-                        Write($"  Género: Música ");
-                        switch (searchedPiece.Gender)
-                        {
-                            case Gender.Classical:
-                                Write("Clásica");
-                                break;
-                            case Gender.Raggeton:
-                                Write("Raggeton");
-                                break;
-                            case Gender.Rock:
-                                Write("Rock");
-                                break;
-                        }
-
-                        WriteLine("");
-
-                        WriteLine($"  Duración: {searchedPiece.Duration} minutos");
-
-                        Write($"  Calidad: ");
-                        switch (searchedPiece.Quality)
-                        {
-                            case Quality.High:
-                                Write("Alta");
-                                break;
-                            case Quality.Low:
-                                Write("Baja");
-                                break;
-                            case Quality.Medium:
-                                Write("Media");
-                                break;
-                        }
-
-                        WriteLine("");
-
-                        Write("  Formato: ");
-                        switch (searchedPiece.Format)
-                        {
-                            case MusicFormat.Mp3:
-                                Write("Mp3");
-                                break;
-                            case MusicFormat.Mp4:
-                                Write("Mp4");
-                                break;
-                        }
-
+                        searchedPiece.Print();
                         WriteLine("");
                     }
                 }
