76427438394e25ff0cdace25f988f0faa60d2454
Lehkikm <Mikhael1729@gmail.com>
2018-12-11 23:05:58 -0800

    feat: Added WriteLine method in ConsoleWriter.

--- Helpers/ConsoleWriter.cs
+++ Helpers/ConsoleWriter.cs
@@ -2,30 +2,42 @@ using static System.Console;
 
 namespace IleanaMusic.Helpers
 {
-    public class ConsoleWriter 
+    public class ConsoleWriter
     {
         public string Spaces { get; private set; }
         public int Indentation { get; set; }
 
-        public ConsoleWriter(int spaceQuantity, int indentation = 3) 
+        public ConsoleWriter(int spaceQuantity, int indentation = 3)
         {
             Spaces = GenerateSpaces(spaceQuantity);
             Indentation = indentation;
         }
 
-        public void Write(string text, int indent = 0, bool spaceBefore = false, bool cancelSpace = false) 
+        public void Write(string text, int indent = 0, bool spaceBefore = false, bool cancelSpace = false)
         {
             var extra = GenerateSpaces(indent * (Spaces.Length > 0 ? Spaces.Length : Indentation));
 
             System.Console.Write(
-                (spaceBefore ? "\n" : "") 
-                + (!cancelSpace ? Spaces : "") 
+                (spaceBefore ? "\n" : "")
+                + (!cancelSpace ? Spaces : "")
                 + extra
-                + text 
+                + text
             );
         }
 
-        string GenerateSpaces(int spaceQuantity) 
+        public void WriteLine(string text, int indent = 0, bool spaceBefore = false, bool cancelSpace = false)
+        {
+            var extra = GenerateSpaces(indent * (Spaces.Length > 0 ? Spaces.Length : Indentation));
+
+            System.Console.WriteLine(
+                (spaceBefore ? "\n" : "")
+                + (!cancelSpace ? Spaces : "")
+                + extra
+                + text
+            );
+        }
+
+        string GenerateSpaces(int spaceQuantity)
         {
             var spaces = "";
 
