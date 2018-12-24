040f49a7be0ac62e5997a351249b7e2f8cf4c3a9
Lehkikm <Mikhael1729@gmail.com>
2018-12-23 19:57:45 -0800

    feat: Added ExcelReportScreen.cs

---
+++ Screens/ExcelReportScreen.cs
@@ -0,0 +1,56 @@
+﻿using IleanaMusic.Data;
+using IleanaMusic.Data.Services;
+using IleanaMusic.Helpers;
+using IleanaMusic.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace IleanaMusic.Screens
+{
+    public class ExcelReportScreen
+    {
+        ConsoleWriter writer = new ConsoleWriter(0);
+        PieceService pieceService = AppData.Instance.PieceService;
+        ReportingHelper reporter = AppData.Instance.ReportingHelper;
+
+        public ExcelReportScreen()
+        {
+            Title();
+
+            var pieces = pieceService.GetAll();
+
+            writer.WriteLine(
+                "Generando reporte..."
+            );
+
+            var successful = reporter.ReportPieces(
+                fileName: "Piezas",
+                pieces: pieces,
+                title: "Reporte de Piezas",
+                type: ReportType.Excel,
+                portrait: false,
+                sheetName: "Piezas"
+            );
+
+            Clear();
+
+            Title();
+            if (successful)
+                writer.WriteLine("¡Reporte generado exitósamente! : )");
+            else
+                writer.WriteLine("Lo sentimos, el reporte no ha podido ser generado. : (");
+        }
+
+        public void Title()
+        {
+            writer.WriteLine(
+                "Reporte en Excel\n" +
+                "--------------\n"
+            );
+        }
+    }
+}
