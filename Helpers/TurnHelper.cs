bbb2a5f81fd8f87d4c8f512c286131c5f1484503
Lehkikm <Mikhael1729@gmail.com>
2018-12-26 15:47:02 -0800

    feat: Added TurnHelper.
    It's to manage console states.

---
+++ Helpers/TurnHelper.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+
+namespace IleanaMusic.Helpers
+{
+    public static class TurnHelper
+    {
+        public static void Pause()
+        {
+            WriteLine("\nPresiona Enter para continuar");
+            ReadLine();
+        }
+    }
+}
